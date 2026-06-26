3e6ecd8fa1e032ac8228b97ce913ed07a0fe9a9d
dikaproject <dikaproject126@gmail.com>
2026-06-26 12:25:21 +0800

    add: dashboard page and all controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\MenuController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,13 @@ Route::get('/cart/clear',[MenuController::class, 'clearCart'])->name('cart.clear
 Route::get('/checkout', [MenuController::class, 'checkout'])->name('checkout');
 Route::post('/checkout/store', [MenuController::class, 'storeOrder'])->name('checkout.store');
 Route::get('/checkout/success/{orderId}', [MenuController::class, 'checkoutSuccess'])->name('checkout.success');
+
+//Admin Routes 
+Route::get('/dashboard', function() {
+    return view('admin.dashboard');
+})->name('dashboard');
+Route::resource('categories', CategoryController::class);
+Route::resource('items', CategoryController::class);
+Route::resource('roles', CategoryController::class);
+Route::resource('users', CategoryController::class);
+Route::resource('orders', CategoryController::class);
